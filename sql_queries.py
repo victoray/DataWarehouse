6c88bfe72560a68aac083bf4423163c8c212cb8e
Victor A <33580233+victoray@users.noreply.github.com>
2019-07-19 02:21:33 -0700

    add sql queries

--- sql_queries.py
+++ sql_queries.py
@@ -5,11 +5,17 @@ import configparser
 config = configparser.ConfigParser()
 config.read('dwh.cfg')
 
+ARN                    = config.get("IAM_ROLE", "ARN")
+S3_LOG_DATA            = config.get("S3", "LOG_DATA")
+S3_LOG_JSONPATH        = config.get("S3", "LOG_JSONPATH")
+S3_SONG_DATA           = config.get("S3", "SONG_DATA")
+
+
 # DROP TABLES
 
 staging_events_table_drop = "DROP TABLE IF EXISTS staging_events"
 staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs"
-songplay_table_drop = "DROP TABLE IF EXISTS song_play"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
 user_table_drop = "DROP TABLE IF EXISTS users"
 song_table_drop = "DROP TABLE IF EXISTS songs"
 artist_table_drop = "DROP TABLE IF EXISTS artists"
@@ -19,23 +25,53 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 
 staging_events_table_create= ("""
 CREATE TABLE staging_events(
+    artist text,
+    auth text,
+    first_name text,
+    gender text,
+    item_in_session int,
+    last_name text,
+    length numeric,
+    level text,
+    location text,
+    method text,
+    page text,
+    registeration numeric,
+    session_id int,
+    song text,
+    status int,
+    ts bigint,
+    user_agent text,
+    user_id int
 )
 """)
 
 staging_songs_table_create = ("""
+CREATE TABLE staging_songs(
+    num_songs int,
+    artist_id text,
+    artist_latitude numeric,
+    artist_longitude numeric,
+    artist_location text,
+    artist_name text,
+    song_id text,
+    title text,
+    duration numeric,
+    year int
+)
 """)
 
 songplay_table_create = ("""
 CREATE TABLE songplays (
-    songplay_id   IDENTITY(0, 1) PRIMARY KEY,
+    songplay_id   int IDENTITY(0, 1) PRIMARY KEY,
     start_time    date not null,
     user_id       int not null,
-    level         text not null,
-    song_id       text not null,
-    artist_id     text not null,
-    session_id    int not null,
-    location      text not null,
-    user_agent    text not null
+    level         text,
+    song_id       text,
+    artist_id     text,
+    session_id    int,
+    location      text,
+    user_agent    text
 )
 """)
 
@@ -63,7 +99,7 @@ artist_table_create = ("""
 CREATE TABLE artists(
     artist_id   text PRIMARY KEY,
     name        text not null,
-    location    text not null,
+    location    text,
     latitude    text,
     longitude   text
 )
@@ -84,26 +120,64 @@ CREATE TABLE time(
 # STAGING TABLES
 
 staging_events_copy = ("""
-""").format()
+COPY staging_events 
+FROM {}
+iam_role {}
+JSON {}
+""").format(S3_LOG_DATA, ARN, S3_LOG_JSONPATH)
 
 staging_songs_copy = ("""
-""").format()
+COPY staging_songs 
+FROM {}
+iam_role {}
+JSON 'auto'
+""").format(S3_SONG_DATA, ARN)
 
 # FINAL TABLES
 
 songplay_table_insert = ("""
+INSERT INTO songplays(start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+SELECT TIMESTAMP 'epoch' + staging_events.ts/1000 * interval '1 second' AS start_time,
+       e.user_id as user_id,
+       e.level as level,
+       s.song_id as song_id,
+       s.artist_id as artist_id,
+       e.session_id as session_id,
+       e.location as location,
+       e.user_agent as user_agent
+FROM staging_events e
+LEFT JOIN staging_songs s ON e.song = s.title
+WHERE e.page = 'NextSong'
 """)
 
 user_table_insert = ("""
+INSERT INTO users(user_id, first_name, last_name, gender, level)
+SELECT DISTINCT user_id, first_name, last_name, gender, level
+FROM staging_events
+WHERE page = 'NextSong'
 """)
 
 song_table_insert = ("""
+INSERT INTO songs (song_id, title, artist_id, year, duration)
+SELECT DISTINCT song_id, title, artist_id, year, duration
+FROM staging_songs
 """)
 
 artist_table_insert = ("""
+INSERT INTO artists (artist_id, name, location, latitude, longitude)
+SELECT DISTINCT artist_id, name, artist_location as location, artist_latitude as latitude, artist_longitude as longitude
+FROM staging_songs
 """)
 
 time_table_insert = ("""
+INSERT INTO time(start_time, hour, day, week, month, year, weekday)
+SELECT DISTINCT TIMESTAMP 'epoch' + ts/1000 * interval '1 second' AS start_time,
+       EXTRACT(hour from ts) AS hour,
+       EXTRACT(day from ts) AS day,
+       EXTRACT(week from ts) AS week,
+       EXTRACT(year from ts) AS year,
+       EXTRACT(dayofweek from ts) AS weekday
+FROM staging_events
 """)
 
 # QUERY LISTS
